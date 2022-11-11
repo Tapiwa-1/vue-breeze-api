37d346e85eb01b926113e04f09179ed77ef6d9e8
Tapiwa Motsi <tmotsi14@gmail.com>
2022-11-11 17:58:42 +0200

    Axios, Pinia, Router added

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router';
+import './axios';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
+app.use(router);
+app.mount('#app');
