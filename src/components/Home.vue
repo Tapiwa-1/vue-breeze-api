4cbd65e01e0696a4c9969d701f5aa08e91990166
Tapiwa Motsi <tmotsi14@gmail.com>
2022-11-15 19:36:36 +0200

    Log in successfull

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,18 @@
 <template>
-  Home
+  <h1>{{ user?.name}}</h1>
+  <p>{{user?.email}}</p>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-}
+const user =ref();
+onMounted(async ()=>{
+  const data = await axios.get('/api/user');
+  user.value = data.data
+});
 </script>
-
 <style>
 
 </style>
