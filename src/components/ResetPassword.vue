0f68d1c3735130de979f2a3a76b04757940877c3
Tapiwa Motsi <tmotsi14@gmail.com>
2022-11-23 08:39:57 +0200

    Reset Password Added

---
+++ src/components/ResetPassword.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        
+          <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+              <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                      Forget Password
+                  </h1>
+                  <form @submit.prevent="authStore.handleResetPassword(form)" class="space-y-4 md:space-y-6" action="#">
+                    <div
+                        class="m-2 p-2 text-green-900 font-semibold bg-green-300 rounded-md"
+                        v-if="authStore.status"
+                        >
+                        {{ authStore.status }}
+                    </div>
+                      <div>
+                          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter New Password</label>
+                          <input type="password" v-model="form.password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                      </div>
+                      <button type="submit" class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Reset Password</button> 
+                  </form>
+              </div>
+          </div>
+      </div>
+    </section>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  import { useAuthStore } from '../stores/auth';
+  import { useRouter } from 'vue-router';
+
+  const route = useRouter();
+  const authStore = useAuthStore();
+  const form = ref({
+    password:"",
+    password_confirmation:"",
+    email: route.query.email,
+    token: route.params.token,
+  });
+  
+  </script>
+  
+  <style>
+  
+  </style>
+  
